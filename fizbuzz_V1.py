8e65a968ae0651e9160d93eda6dd9f3d7e7c3032
Suyesh Bhandari <suyeshb@gmail.com>
2014-06-02 07:29:20 -0400

    update fizzbuzz version 2

--- fizbuzz_V1.py
+++ fizbuzz_V1.py
@@ -1,9 +1,24 @@
-for i in range(1,100):
-	if i % 3 == 0 and i % 5 == 0:
+import sys
+
+while len(sys.argv) <= 1:
+	number = raw_input("Enter any number: ")
+	number = int(number)
+	if number % 3 == 0 and number % 5 == 0:
 		print "FizzBuzz"
-	elif i % 3 == 0:
+	elif number % 3 == 0:
 		print "Fizz"
-	elif i % 5 == 0:
+	elif number % 5 == 0:
 		print "Buzz"
 	else:
-		print i
+		print number
+else:
+	for num in sys.argv[1:]:
+		if int(num) % 3 == 0 and int(num) % 5 == 0:
+			print "FizzBuzz"
+		elif int(num) % 3 == 0:
+			print "Fizz"
+		elif int(num) % 5 == 0:
+			print "Buzz"
+		else:
+			print num
+		
